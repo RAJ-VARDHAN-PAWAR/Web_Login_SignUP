d2aaeddd447f8f6f200a4d5c67c9b473c255cbb4
RAJ-VARDHAN-PAWAR <b.rajvardhanpawar@gmail.com>
2026-09-09 09:44:19 +0530

    Search feature

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import './Home.css'
 
 function Home() {
   const [products, setProducts] = useState([])
+  const [search, setSearch] = useState("")
 
   function fetchData() {
     fetch("https://dummyjson.com/products")
@@ -22,24 +23,78 @@ function Home() {
     fetchData()
   }, [])
 
+  const filteredProducts = products.filter((product) =>
+    product.title.toLowerCase().includes(search.toLowerCase())
+  )
+
   return (
-    <div className="products-container">
-      {products.map((product) => (
-        <div className="product-card" key={product.id}>
+    <div>
 
-          <img src={product.thumbnail} alt={product.title} className="product-image"/>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search products..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
 
-          <div className="product-details">
-            <h2>{product.title}</h2>
-            <p>{product.description}</p>
-            <p><strong>Price:</strong> ${product.price}</p>
-            <p><strong>Category:</strong> {product.category}</p>
-            <p><strong>Brand:</strong> {product.brand}</p>
-            <p><strong>Rating:</strong> ⭐ {product.rating}</p>
-            <p><strong>Stock:</strong> {product.stock}</p>
+      <select>
+        {
+            products.map((product)=>{
+                return(
+                    <option>{product.category}k</option>
+                )
+            })
+        }
+      </select>
+ 
+      <div className="products-container">
+
+        {filteredProducts.map((product) => (
+
+          <div className="product-card" key={product.id}>
+
+            <img
+              src={product.images[0]}
+              alt={product.title}
+              className="product-image"
+            />
+
+            <div className="product-details">
+
+              <h2>{product.title}</h2>
+
+              <p>{product.description}</p>
+
+              <p>
+                <strong>Price:</strong> ${product.price}
+              </p>
+
+              <p>
+                <strong>Category:</strong> {product.category}
+              </p>
+
+              <p>
+                <strong>Brand:</strong> {product.brand}
+              </p>
+
+              <p>
+                <strong>Rating:</strong> {product.rating}
+              </p>
+
+              <p>
+                <strong>Stock:</strong> {product.stock}
+              </p>
+
+            </div>
+
           </div>
-        </div>
-      ))}
+
+        ))}
+
+      </div>
+
     </div>
   )
 }
